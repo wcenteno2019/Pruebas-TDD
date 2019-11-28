f617e0152bbb35d6cb73ae5343cfa1c6db93450f
Luis <luise.castaneda@avianca.com>
2019-11-28 16:36:25 -0600

    Endpoint funcional
    Endpoint funcional

--- src/main/java/com/loyalty/proyectoTDD/controller/ClientController.java
+++ src/main/java/com/loyalty/proyectoTDD/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.loyalty.proyectoTDD.controller;
 
 import com.loyalty.proyectoTDD.interfaces.IProcess;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/client-info")
 public class ClientController {
     private IProcess<Boolean,String> clientProcess;
+
+    @Autowired
+    public ClientController(IProcess<Boolean, String> clientProcess) {
+        this.clientProcess = clientProcess;
+    }
+
+
     @RequestMapping(method = RequestMethod.GET, value = "/{idCliente}")
     public boolean clientExist(@PathVariable String idCliente){
         return clientProcess.process(idCliente);
